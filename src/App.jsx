4d9a3d83d2084c017d244f38d3283ad2324af477
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-03-11 20:54:02 -0600

    Create wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Header from './components/Header';
 import { Hero } from './components/Hero';
+import { Wrapper } from './components/Wrapper';
 
 function App() {
   return (
     <div>
       <Header />
-      <Hero />
+      <Wrapper>
+        <Hero />
+      </Wrapper>
     </div>
   );
 }
---
+++ src/components/Wrapper.jsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  max-inline-size: 78rem;
+  margin: 0 auto;
+  padding-inline-end: 1.5rem;
+  padding-inline-start: 1.5rem;
+`;
